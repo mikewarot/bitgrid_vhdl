a2b329d69d8563bffa710b13bba6a99ee41d1a9d
Mike Warot <chezmike@gmail.com>
2024-01-11 10:03:17 -0600

    input bits now actually index LUT1

--- BitGridTb.vhd
+++ BitGridTb.vhd
@@ -1,25 +1,28 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity BitGridTb is
 
   signal inputs   : std_logic_vector(3 downto 0) := (others => '0');
   signal outputs  : std_logic_vector(3 downto 0) := (others => '0');
   signal clock    : std_logic := '0';
-  signal LUT1     : std_logic_vector(15 downto 0) := (others => '0');
+  signal LUT1     : std_logic_vector(15 downto 0) := "1111111100000000";
 
 end entity;
 
 architecture sim of BitGridTb is
+    signal n : integer;
 
 begin
 
   -- Shift register
   process(clock)
   begin
+    n <= to_integer(unsigned(inputs));  
     if rising_edge(clock) then
       for i in 3 downto 0 loop
-        outputs(i) <= inputs(i);
+        outputs(i) <= LUT1(n);
       end loop;
     end if;
   end process;
